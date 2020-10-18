fc275dd9629ba2af22329ffa5fc2d8d6a06e1352
Ricky <rickdhanota@gmail.com>
2020-10-17 18:31:37 -0700

    annotated and took some notes

--- insertion_sort_easy.py
+++ insertion_sort_easy.py
@@ -4,12 +4,15 @@
 #output = [2, 3, 5, 5, 6, 8, 9]
 
 def insertionSort(array):
-    for i in range(1, len(array)):
+    for i in range(1, len(array)): #We start at 1 because we're basically creating a sub array within the array which consists of 1 value at the start. We then look at every proceeding value and sort it within the sub array. We're not, however, actually creating a sub array.
         j = i
         while j > 0 and array[j] < array[j - 1]:
             # swap(j, j-1, array)
             array[j], array[j - 1] = array[j - 1], array[j]
-            j -= 1
+            j -= 1 #we do j -= 1 because we want it to sort with the preceeding numbers and not the proceeding numbers because that would be incorrect. basically loop forward with the iniital for loop and then loop backwards with this while loop
     return array
+
+# def swap(i, j, array):
+#     array[i], array[j] = array[j], array[i]
 
 print(insertionSort([8, 5, 2, 9, 5, 6, 3]))
